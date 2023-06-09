b778b112f114faf377565e56a88de3dfc56e23b9
Tanyababinets <tanya060616@gmail.com>
2023-06-09 16:10:24 +0300

    Add project files.

---
+++ Keyboard2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Keyboard2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool flag = false;
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
